449fa02cc08b4c23d10eb5bb159cf07ca9f1b24f
glacials <qhiiyr@gmail.com>
2020-10-31 23:42:20 -0700

    Fix version number generation

--- release.sh
+++ release.sh
@@ -6,6 +6,6 @@ echo ":rubygems_api_key: $RUBYGEMS_API_KEY" >> ~/.gem/credentials
 echo ":github: Bearer $GITHUB_PERSONAL_ACCESS_TOKEN" >> ~/.gem/credentials
 chmod 0600 ~/.gem/credentials
 rm -f *.gem
-gem build friendly_uuid.gemspec
+gem build friendly_uuid
 gem push *.gem
 gem push *.gem --key github --host https://rubygems.pkg.github.com/glacials
